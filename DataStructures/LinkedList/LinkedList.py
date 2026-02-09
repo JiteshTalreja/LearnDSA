37549364e4f4ff8d4e0d0c32ff5f003e4e545911
jiteshtalreja <jitesh.talreja101@gmail.com>
2026-02-09 08:58:46 +0530

    LinkedList implementation

--- DataStructures/LinkedList/LinkedList.py
+++ DataStructures/LinkedList/LinkedList.py
@@ -78,7 +78,7 @@ class LinkedList:
             self.head = newNode
             self.tail = newNode
         else:
-            self.head.next = self.head
+            newNode.next = self.head
             self.head = newNode
         self.length +=1
         return True
@@ -101,3 +101,54 @@ class LinkedList:
         for _ in range(index):
             temp = temp.next
         return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert_value(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index ==self.length:
+            return self.append(value)
+
+        temp = self.get(index - 1)
+        new_node = Node(value)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length +=1
+        return True
+
+    def remove(self, index):
+        if index<0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index ==self.length:
+            return self.pop()
+
+        prev = self.get(index-1)
+        temp = prev.next
+
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+    ## Very, Very common interview questions
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        before = None
+        after = temp.next
+        for _ in range(self.length):
+            after =temp.next
+            temp.next = before
+            before = temp
+            temp = after
