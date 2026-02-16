fa9a7793c35bea1319fb1ba008181dcb5d9132db
jiteshtalreja <jitesh.talreja101@gmail.com>
2026-02-16 10:14:57 +0530

    doubly linked list

--- DataStructures/DoublyLinkedList/DoublyLinkedList.py
+++ DataStructures/DoublyLinkedList/DoublyLinkedList.py
@@ -43,5 +43,47 @@ class DoublyLinkedList:
             self.tail = None
         return temp
 
+    def prepend(self, value):
+
+        newNode = Node(value)
+
+        if self.length == 0:
+            self.head = self.tail = newNode
+
+        else:
+            newNode.next = self.head
+            self.head.prev = newNode
+            self.head = newNode
+        self.length += 1
+        return True
+
+    def pop_first(self):
+
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
+
+    def get(self, index):
+
+        if index <0 or index > self.length:
+            return None
+        if index < self.length //2:
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length-1, index, -1):
+                temp = temp.prev
+        return temp
 
 
