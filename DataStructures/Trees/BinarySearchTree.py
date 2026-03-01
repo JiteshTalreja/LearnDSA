c3ea836feb19d9f07a80995ef9c09532c0a882ef
jiteshtalreja <jitesh.talreja101@gmail.com>
2026-03-01 16:38:05 +0530

    insert in Binary search trees

--- DataStructures/Trees/BinarySearchTree.py
+++ DataStructures/Trees/BinarySearchTree.py
@@ -10,3 +10,35 @@ class BinarySearchTree:
 
     def __init__(self):
         self.root = None
+
+    def insert(self, value):
+
+        new_node = Node(value)
+
+        if not self.root:
+            self.root = new_node
+            return True
+        temp = self.root
+        while True:
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if not temp.left:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if not temp.right:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+tree = BinarySearchTree()
+
+tree.insert(2)
+tree.insert(1)
+tree.insert(3)
+
+print(tree.root.value)
+print(tree.root.left.value)
+print(tree.root.right.value)
