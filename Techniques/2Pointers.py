f0fc422a19bd0270cc63140098ccafc9b220a724
jiteshtalreja <jitesh.talreja101@gmail.com>
2026-03-07 12:54:18 +0530

    2 pointers

--- Techniques/2Pointers.py
+++ Techniques/2Pointers.py
@@ -199,3 +199,47 @@ def good_partition2(arr):
         j = max(j, dict1[arr[i]])
         i +=1
     return result
+
+
+""""
+## 1750. Minimum Length of String After Deleting Similar Ends
+
+Given a string s consisting only of characters 'a', 'b', and 'c'. You are asked to apply the following algorithm on the string any number of times:
+
+Pick a non-empty prefix from the string s where all the characters in the prefix are equal.
+Pick a non-empty suffix from the string s where all the characters in this suffix are equal.
+The prefix and the suffix should not intersect at any index.
+The characters from the prefix and suffix must be the same.
+Delete both the prefix and the suffix.
+Return the minimum length of s after performing the above operation any number of times (possibly zero times).
+
+
+
+Example 1:
+
+Input: s = "ca"
+Output: 2
+Explanation: You can't remove any characters, so the string stays as is.
+
+Example 2:
+
+Input: s = "cabaabac"
+Output: 0
+Explanation: An optimal sequence of operations is:
+- Take prefix = "c" and suffix = "c" and remove them, s = "abaaba".
+- Take prefix = "a" and suffix = "a" and remove them, s = "baab".
+- Take prefix = "b" and suffix = "b" and remove them, s = "aa".
+- Take prefix = "a" and suffix = "a" and remove them, s = "".
+"""
+
+def min_length(string):
+    i = 0
+    j = len(string) -1
+    while i < j and string[i] == string[j]:
+        char = string[i]
+        while i < j and string[i] == char:
+            i +=1
+        while j >= i and string[j] == char:
+            j -=1
+
+    return j - i +1
