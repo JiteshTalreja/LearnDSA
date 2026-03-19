94f17b1f47ddb39a6080946b000821339f06532d
jiteshtalreja <jitesh.talreja101@gmail.com>
2026-03-19 12:45:11 +0530

    recursion on trees

--- DataStructures/Trees/RecursiveBinarySearchTree.py
+++ DataStructures/Trees/RecursiveBinarySearchTree.py
@@ -37,7 +37,39 @@ class BinarySearchTree:
             current_node.right = self.__r_insert(current_node.right, value)
         return current_node
 
-    def _r_insert(self, value):
+    def r_insert(self, value):
         if self.root == None:
             self.root = Node(value)
         self.__r_insert(self.root,value)
+
+
+    def min_value(self, current_node):
+
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+
+
+    def __delete_node(self, current_node, value):
+        if current_node == None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left == None and current_node.right == None:
+                return None
+            elif current_node.left == None:
+                current_node = current_node.right
+            elif current_node.right == None:
+                current_node = current_node.left
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+        return current_node
+
+
+    def delete_node(self, value):
+        self.root = self.__delete_node(self.root, value)
