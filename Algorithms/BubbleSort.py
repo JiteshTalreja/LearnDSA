7f956eaaed7936c06c67c1489e677e89854e16f2
jiteshtalreja <jitesh.talreja101@gmail.com>
2026-03-21 13:32:19 +0530

    sorting

---
+++ Algorithms/BubbleSort.py
@@ -0,0 +1,13 @@
+def bubble_sort(nums):
+
+    for i in range(len(nums)-1, 0, -1):
+        for j in range(i):
+            if nums[j] > nums[j+1]:
+
+                ## SWAP
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+    return nums
+
+
+
+print(bubble_sort([4,2,6,5,1,3]))
