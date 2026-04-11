9eb41bdcb287d6f2dd6045ea8a70416fdef3bf8f
jiteshtalreja <jitesh.talreja101@gmail.com>
2026-04-11 20:23:49 +0530

    sorting revision

--- Strivers/Sorting/Sorting.py
+++ Strivers/Sorting/Sorting.py
@@ -16,4 +16,33 @@ def selection_sort(nums):
     return nums
 
 
-print(selection_sort([5,6,4,7,2,1,3]))
+print('selection: ', selection_sort([5,6,4,7,2,1,3]))
+
+
+
+def bubble_sort(nums):
+    for i  in range(len(nums)-1, 0, -1):
+        for j in range(i):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+        
+    return nums
+
+print('bubble: ', bubble_sort([5,6,4,7,2,1,3]))
+
+
+def insertion_sort(nums):
+
+    for i in range(1, len(nums)):
+        key = nums[i]
+
+        j = i-1
+
+        while j >=0 and nums[j] > key:
+            nums[j+1] = nums[j]
+            nums[j] = key
+            j-=1
+    return nums
+
+
+print('insertion: ', insertion_sort([5,6, 4, 7, 2, 3, 1]))
