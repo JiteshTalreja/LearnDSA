ece338eee873cffc8f0db29e423ec494277cdef5
jiteshtalreja <jitesh.talreja101@gmail.com>
2026-04-13 11:19:27 +0530

    Strivers Arrays

---
+++ Strivers/ProblemSolving/Arrays.py
@@ -0,0 +1,37 @@
+## Q1 Find the largest number in an unsorted Array
+
+
+## solution Brute force to sort the array.
+
+## Optimal solution is below
+
+def largest(nums):
+
+    maxi = nums[0]
+
+    for i in nums:
+        if i > maxi:
+            maxi = i
+    return maxi
+
+## Q2 second largest in a unsorted array
+
+## AGAIN Brute force is to sort the array
+
+
+def slargest(nums):
+
+    ## First find the largest
+    largest = nums[0]
+
+    for i in nums:
+        if i > largest:
+            largest = i
+    slargest = -1
+
+    for i in nums:
+        if i > slargest and i != largest:
+            slargest = i
+    return slargest
+
+print(slargest([3,20,10,15,2]))
