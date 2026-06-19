9a180e9e4d98d1f0526be199a9e50d8152e7be16
jiteshtalreja <jitesh.talreja101@gmail.com>
2026-06-19 10:26:55 +0530

    Striver's A2Z

--- Strivers/ProblemSolving/BinarySearch.py
+++ Strivers/ProblemSolving/BinarySearch.py
@@ -469,7 +469,7 @@ print('first and last occurrence BS: ', first_last_bs([2, 4, 6, 8, 8, 8, 11, 13]
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------
-##Q5 BS-4. Search Element in Rotated Sorted Array - I
+##Q5 BS-4. Search Element in Rotated Sorted Array - I (unique elements)
 
 def search_rot_array(nums, target):
     left = 0; right = len(nums)-1
@@ -492,6 +492,35 @@ def search_rot_array(nums, target):
             else:
                 right =mid-1
     return -1
-
-
 print("search in rotate sorted array: ", search_rot_array([7,8,9,1,2,3,4,5,6], 1))
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------
+##Q6 BS-5. Search Element in Rotated Sorted Array II (can have duplicates)
+
+def search_dup_rot_array(nums, target):
+    left = 0; right = len(nums)-1
+
+    while left<= right:
+        mid = left+(right-left)//2
+
+        if nums[mid]==target:
+            return True
+        if nums[left] == nums[mid] == nums[right]:
+            left+=1
+            right-=1
+            continue
+        # left sorted
+        if nums[left] <= nums[mid]:
+            if nums[left]<= target <=nums[mid]:
+                right = mid-1
+            else:
+                left = mid+1
+        # right sorted
+        else:
+            if nums[mid]<=target<=nums[right]:
+                left = mid+1
+            else:
+                right = mid-1
+    return False
+
