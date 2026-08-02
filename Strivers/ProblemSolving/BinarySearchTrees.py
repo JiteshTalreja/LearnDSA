1aa076e7799ffefb1c51361c19da9a9745ecf726
jiteshtalreja <jitesh.talreja101@gmail.com>
2026-08-02 13:49:02 +0530

    Strivers A2Z

--- Strivers/ProblemSolving/BinarySearchTrees.py
+++ Strivers/ProblemSolving/BinarySearchTrees.py
@@ -558,6 +558,30 @@ def max_depth(root):
     return 1+max(lmax, rmax)
 
 #######################################################
+# L15. Check for Balanced Binary Tree
+#######################################################
+
+def is_balanced(root):
+
+    def dfs(node):
+        if node is None:
+            return 0
+
+        left = dfs(node.left)
+        if left == -1:
+            return -1
+
+        right = dfs(node.right)
+        if right == -1:
+            return -1
+
+        if abs(left - right) > 1:
+            return -1
+
+        return 1+max(left, right)
+    return dfs(root) != -1
+
+#######################################################
 # Example Tree
 #######################################################
 
